d37086a1e43337c4d848a0828ae3e0ab31013dd2
宇根昇汰 <une@unenoborunoMacBook-Air.local>
2023-03-31 14:20:30 +0900

    UIの修正

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -11,6 +11,9 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final double deviceWidth = MediaQuery.of(context).size.width;
+
     // state（状態）
     final _homePageState = ref.watch(homePageProvider);
     // provider（状態の操作）
@@ -22,68 +25,83 @@ class HomePage extends ConsumerWidget {
       body: SizedBox(
           width: double.infinity,
           child: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage('images/mockKYOUTAI.png'),
-            )),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                      width: 400,
-                      height: 200,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+              alignment: Alignment.center,
+              child: AspectRatio(
+                  aspectRatio: 3 / 4,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.blue,
+                        image: DecorationImage(
+                          image: AssetImage('images/mockKYOUTAI.png'),
+                        )),
+                    child: Stack(
+                        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Expanded(
-                              child: Visibility(
-                                  maintainSize: true,
-                                  maintainState: true,
-                                  maintainAnimation: true,
-                                  visible: _homePageState.visibleL,
-                                  child: Image.asset(
-                                      images.items[_homePageState.indexL]))),
-                          Expanded(
-                              child: Visibility(
-                                  maintainSize: true,
-                                  maintainState: true,
-                                  maintainAnimation: true,
-                                  visible: _homePageState.visibleC,
-                                  child: Image.asset(
-                                      images.items[_homePageState.indexC]))),
-                          Expanded(
-                              child: Visibility(
-                                  maintainSize: true,
-                                  maintainState: true,
-                                  maintainAnimation: true,
-                                  visible: _homePageState.visibleR,
-                                  child: Image.asset(
-                                      images.items[_homePageState.indexR]))),
-                        ],
-                      )),
-                  Container(
-                      alignment: Alignment.bottomRight,
-                      width: 600,
-                      height: 150,
-                      child: Container(
-                          padding: const EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color: Colors.black26,
-                            border: Border.all(color: Colors.black45),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: InkResponse(
-                            onTap: () {
-                              _homePageNotifier.loopLottery();
-                            },
-                            child: Transform.rotate(
-                              angle: _homePageState.handleAngle * pi / 180,
-                              child: Image.asset("images/handle.png"),
-                            ),
-                          )))
-                ]),
-          )),
+                          Positioned(
+                              top: deviceWidth * 0.2,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Visibility(
+                                    maintainSize: true,
+                                    maintainState: true,
+                                    maintainAnimation: true,
+                                    visible: _homePageState.visibleL,
+                                    child: Container(
+                                        decoration: BoxDecoration(
+                                          border: Border.all(color: Colors.red),
+                                        ),
+                                        height: deviceWidth * 0.4,
+                                        child: Image.asset(images
+                                            .items[_homePageState.indexL])),
+                                  ),
+                                  Visibility(
+                                      maintainSize: true,
+                                      maintainState: true,
+                                      maintainAnimation: true,
+                                      visible: _homePageState.visibleC,
+                                      child: Container(
+                                          height: deviceWidth * 0.4,
+                                          child: Image.asset(images
+                                              .items[_homePageState.indexC]))),
+                                  Visibility(
+                                      maintainSize: true,
+                                      maintainState: true,
+                                      maintainAnimation: true,
+                                      visible: _homePageState.visibleR,
+                                      child: Container(
+                                          height: deviceWidth * 0.4,
+                                          child: Image.asset(images
+                                              .items[_homePageState.indexR]))),
+                                ],
+                              )),
+                          Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Container(
+                                  height: deviceWidth * 0.4,
+                                  padding: const EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black26,
+                                    border: Border.all(color: Colors.black45),
+                                    borderRadius: const BorderRadius.horizontal(
+                                        left: Radius.circular(20)),
+                                  ),
+                                  child: InkResponse(
+                                    onTap: () {
+                                      _homePageNotifier.loopLottery();
+                                    },
+                                    child: Transform.rotate(
+                                      angle:
+                                          _homePageState.handleAngle * pi / 180,
+                                      child: Expanded(
+                                          child:
+                                              Image.asset("images/handle.png")),
+                                    ),
+                                  )))
+                        ]),
+                  )))),
     );
   }
 }
